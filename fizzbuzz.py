a73f80bce45feebd6b9135409996fcc8af1976f3
Abby C <54291691+abbyjc42@users.noreply.github.com>
2022-10-14 23:26:49 -0500

    Updated Code for Readability
    Code was cleaned up and formatted for improved readability, leftover code from previous iteration was removed. Comments were added to explain reasoning and sources for some self-contained documentation.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,21 +1,27 @@
-# Branch 1: Efficiency
-
-# "Write a program that prints the numbers from 1 to 100.
-# But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
-# For numbers which are multiples of both three and five print “FizzBuzz”.
+# Branch 3: Readability
 
 def main():
-    numbers = [n for n in range(1, 101)]
-    for number in numbers:
-        if number %5 == 0 and number % 3 == 0:
+    # Print numbers in range 1 to 100
+    num_range = [n for n in range(1, 101)]
+    for i in num_range:
+        if i % 5 == 0 and i % 3 == 0:   # If divisible by 3 and 5, print 'FizzBuzz'
             print("FizzBuzz")
-        elif number % 3== 0:
+        elif i % 3 == 0:    # If divisible by 3 (not 5), print 'Fizz'
             print("Fizz")
-        elif number% 5 ==0:
+        elif i % 5 == 0:    # If divisible by 5 (not 3), print 'Buzz'
             print("Buzz")
-        else:
-            print(number)
-        number = number+ 1
+        else:   # If divisible by neither 3 nor 5, 
+            print(i)
+
 main()
 
-# Source: https://betterprogramming.pub/python-loops-performance-compared-the-fastest-is-b4638744a1ff
+# -------------------------
+#  Notes and References
+# -------------------------
+
+# "i" was used in place of "number" because context makes the variable's purpose evident and its use
+# is confined to the scope of a loop. For those reasons, in this case the decision was made to shorten
+# the variable for cleaner code; otherwise, a more descriptive variable would be used.
+
+# foreach was chosen in place of while due to better performance.
+# See source on loop comparison: https://betterprogramming.pub/python-loops-performance-compared-the-fastest-is-b4638744a1ff
