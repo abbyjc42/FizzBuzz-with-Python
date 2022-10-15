8b453d307b2c5dbd724bd03c7a67156202eee5ce
Abby C <54291691+abbyjc42@users.noreply.github.com>
2022-10-14 22:46:28 -0500

    Created Base FizzBuzz Functionality
    Wrote a basic working program to follow the requirements of FizzBuzz. The generated output shows that the program functions as desired.

---
+++ fizzbuzz.py
@@ -0,0 +1,24 @@
+# Branch 1: Functionality
+
+# "Write a program that prints the numbers from 1 to 100.
+# But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
+# For numbers which are multiples of both three and five print “FizzBuzz”.
+
+number = 1
+while number <=100:
+    if number %5 == 0:
+        if number% 3 == 0:
+            print("FizzBuzz")
+        else:
+            print("Buzz")
+    elif number % 3== 0:
+        if number % 5 == 0:
+            print("FizzBuzz")
+        else:
+            print("Fizz")
+    else:
+        print(i)
+    i = i+ 1
+
+# Note: Given the simplicity of the assignment and the requirement to use 3 branches, the code here
+# was written as it was in order to give myself a baseline to improve on.
